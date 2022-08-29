4b698c3db8f08ebbc480c9411d2d061eb8b94355
mozixreality <mozixreality@gmail.com>
2022-08-29 14:34:04 +0800

    partition

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ seed = 0
 lam = 0.0
 neg = 0
 topk = 0
+part = 0
 
 def mf_bpr_update(Vu, Vi, Vj):
     negxuij = (np.dot(Vu, Vi) - np.dot(Vu, Vj)) * -1        #   Negative of (xui - xuj)
@@ -55,19 +56,37 @@ def bpr_matrix_factorization(P, Q, pos_lst, neg_mat):
 
 def pre_process():
     logger.info('Pre Processing')
-    movie_i2id = {}
-    movie_id2i = {}
-    mov_idx = 0
-    user_i2id = {}
-    user_id2i = {}
-    usr_idx = 0
+    # counting total lines
+    with open(ml_folder + "ratings.dat") as fp:
+        for total_lines, _ in enumerate(fp):
+            pass
+    total_lines += 1
+
+    Ns = []
+    Ms = []
+    user_i2ids = []
+    movie_i2ids = []
+    pos_lsts = []
+    neg_mats = []
 
     logger.info('Building rating matrix')
-    buf_ht = {}
-    pos_lst = []
+    from_p, to_p, p_cnt, p_part = 0, total_lines//part, 0, 0
     with open(ml_folder + "ratings.dat") as file:
         line = file.readline()
         while(line):
+            # partition log info
+            # initialize
+            if p_cnt == from_p:
+                logger.info('building from {} to {}, total {}'.format(from_p, to_p, total_lines))
+                movie_i2id = {}
+                movie_id2i = {}
+                mov_idx = 0
+                user_i2id = {}
+                user_id2i = {}
+                usr_idx = 0
+                buf_ht = {}
+                pos_lst = []
+
             u_id, m_id, rate, timestamp = line.split("::")
 
             if u_id not in user_id2i:
@@ -86,48 +105,68 @@ def pre_process():
             buf_ht[(u_i, m_i)] = 1
 
             line = file.readline()
+
+            # handling partition offset
+            p_cnt += 1
+            if p_cnt == to_p:
+                p_part += 1
+                from_p = total_lines*p_part//part
+                to_p = total_lines*(p_part+1)//part
     
-    N = len(user_id2i)
-    M = len(movie_id2i)
-    neg_mat = [[]] * N
+                N = len(user_id2i)
+                M = len(movie_id2i)
+                Ns.append(N)
+                Ms.append(M)
+                user_i2ids.append(user_i2id)
+                movie_i2ids.append(movie_i2id)
+                pos_lsts.append(pos_lst)
+                neg_mat = [[]] * N
 
-    logger.info('Converting matrix')
-    for i in tqdm(range(N)):
-        buf = []
-        for j in range(M):
-            if (i, j) not in buf_ht:
-                buf.append(j)
-        neg_mat[i] = buf
+                logger.info('Converting matrix')
+                for i in tqdm(range(N)):
+                    buf = []
+                    for j in range(M):
+                        if (i, j) not in buf_ht:
+                            buf.append(j)
+                    neg_mat[i] = buf
+                neg_mats.append(neg_mat)
 
-    return N, M, pos_lst, neg_mat, user_i2id, movie_i2id
+    return Ns, Ms, pos_lsts, neg_mats, user_i2ids, movie_i2ids
 
-def recommend(nR, user_i2id, movie_i2id):
+def recommend(nRs, user_i2ids, movie_i2ids):
     logger.info("Recommend")
     f = open(recommend_file, "w")
     f.write("")
     f.close()
 
-    for i, r in enumerate(tqdm(nR)):
-        movies = np.argpartition(r, -topk)[-topk:][::-1]
-        f = open(recommend_file, "a")
-        for k in range(topk):
-            f.write("{userID}::{movieID}::{rank}\n".format(userID=user_i2id[i], movieID=movie_i2id[int(movies[k])], rank=k+1))
-        f.close()
+    for p in range(part):
+        logger.info("Recommend at part {}, total {}".format(p+1, part))
+        nR = nRs[p]
+        for i, r in enumerate(tqdm(nR)):
+            movies = np.argpartition(r, -topk)[-topk:][::-1]
+            f = open(recommend_file, "a")
+            for k in range(topk):
+                f.write("{userID}::{movieID}::{rank}\n".format(userID=user_i2ids[p][i], movieID=movie_i2ids[p][int(movies[k])], rank=k+1))
+            f.close()
 
 
 def main():
-    N, M, pos_lst, neg_mat, user_i2id, movie_i2id = pre_process()
- 
-    np.random.seed(seed)
-    P = np.random.rand(N,K)
-    Q = np.random.rand(M,K)
-    
-    nP, nQ = bpr_matrix_factorization(P, Q, pos_lst, neg_mat)
+    Ns, Ms, pos_lsts, neg_mats, user_i2ids, movie_i2ids = pre_process()
 
-    nR = np.dot(nP, nQ.T)
-    recommend(nR, user_i2id, movie_i2id)
+    nRs = []
+    for p in range(part):
+        np.random.seed(seed)
+        P = np.random.rand(Ns[p], K)
+        Q = np.random.rand(Ms[p], K)
+        
+        nP, nQ = bpr_matrix_factorization(P, Q, pos_lsts[p], neg_mats[p])
 
-    print(nR)
+        nR = np.dot(nP, nQ.T)
+        nRs.append(nR)
+        print(nR)
+
+    recommend(nRs, user_i2ids, movie_i2ids)
+
 
 if __name__ == '__main__':
 
@@ -136,13 +175,14 @@ if __name__ == '__main__':
     parser.add_argument("-p", "--path", help="Specify dataset path", default="./ml-10m/")
     parser.add_argument("-g", "--graph", help="Specify output graph filename and title", default="ML-1M")
     parser.add_argument("-d", "--dim", help="Set Dimension", type=int, default=10)
-    parser.add_argument("-e", "--epoch", help="Set epochs to be trained", type=int, default=1000)
+    parser.add_argument("-e", "--epoch", help="Set epochs to be trained", type=int, default=500)
     parser.add_argument("-lr", "-a", "--alpha", "--learningrate", help="Set learning rate(Alpha)", type=float, default=0.01)
     parser.add_argument("-lam", help="Set lambda", type=float, default=0.1)
     parser.add_argument("-n", "--neg", help="Set sample times per epoch", type=int, default=100)
     parser.add_argument("-s", "--seed", help="Set random seed", type=int, default=0)
     parser.add_argument("-rcmd", "--rcmd", help="Set recommend file path", default="recommend.dat")
     parser.add_argument("-topk", "--topk", help="Set recommend top k elements", type=int, default=10)
+    parser.add_argument("-part", "--part", help="Split data set into k part", type=int, default=4)
 
     args = parser.parse_args()
 
@@ -156,5 +196,6 @@ if __name__ == '__main__':
     seed = args.seed
     recommend_file = args.rcmd
     topk = args.topk
+    part = args.part
     
     main()
